f6ad64602c4a8295935a3e21db062b5a4f39cfac
SeoSang <ddrrpg@naver.com>
2020-01-04 21:26:59 +0900

    망함

--- js/search.js
+++ js/search.js
@@ -21,16 +21,16 @@ function saveURL() {
 }
 
 function appendHtml(el, str) {
-  var div = document.createElement('div')
-  div.innerHTML = str;
-  
-  while(div.childeren.length > 0) {
+  var div = document.createElement("div")
+  div.innerHTML = str
+
+  while (div.childeren.length > 0) {
     el.appendChild(div.children[0])
   }
 }
 
 document.getElementById("search_input").onblur = function() {
-  searchBasic = search_input.value;
+  searchBasic = search_input.value
 
   // var html = '<div id="search_fake_input">' + searchBasic + '</div>'
   // appendHtml(document.body, html)
@@ -47,7 +47,7 @@ document.getElementById("search_input").onblur = function() {
 }
 
 document.getElementById("search_contain").onblur = function() {
-  if(search_contain.value) {
+  if (search_contain.value) {
     search_contain_input.value = search_contain.value
 
     // width 차후 수정
@@ -59,72 +59,58 @@ document.getElementById("search_contain").onblur = function() {
     search_contain_input.size = len
     console.log(search_contain_input.size)
 
-    search_contain_input.style.zIndex = 100;
-//    alert(document.getElementById("search_contatin_input").style.zIndex)
-  }
-  else {
-    search_contain_input.style.zIndex = 1;
+    search_contain_input.style.zIndex = 100
+    //    alert(document.getElementById("search_contatin_input").style.zIndex)
+  } else {
+    search_contain_input.style.zIndex = 1
   }
 }
 
 document.getElementById("search_except").onblur = function() {
-  if(search_except.value) {
+  if (search_except.value) {
     search_except_input.value = search_except.value
-//    search_except_input.size = search_except.value.length
-    search_except_input.style.zIndex = 100;
-  }
-  else {
-    search_except_input.style.zIndex = 1;
+    //    search_except_input.size = search_except.value.length
+    search_except_input.style.zIndex = 100
+  } else {
+    search_except_input.style.zIndex = 1
   }
 }
 
 document.getElementById("search_synonym").onblur = function() {
-  if(search_synonym.value) {
+  if (search_synonym.value) {
     search_synonym_input.value = search_synonym.value
     search_synonym_input.size = search_synonym.value.length
-    search_synonym_input.style.zIndex = 100;
-  }
-  else {
-    search_synonym_input.style.zIndex = 1;
+    search_synonym_input.style.zIndex = 100
+  } else {
+    search_synonym_input.style.zIndex = 1
   }
 }
 
 document.getElementById("search_filetype").onblur = function() {
-  if(search_filetype.value) {
+  if (search_filetype.value) {
     search_filetype_input.value = search_filetype.value
     search_filetype_input.size = search_filetype.value.length
-    search_filetype_input.style.zIndex = 100;
-  }
-  else {
-    search_filetype_input.style.zIndex = 1;
+    search_filetype_input.style.zIndex = 100
+  } else {
+    search_filetype_input.style.zIndex = 1
   }
 }
 
 document.getElementById("search_site").onblur = function() {
-  if(search_site.value) {
+  if (search_site.value) {
     search_site_input.value = search_site.value
     search_site_input.size = search_site.value.length
-    search_site_input.style.zIndex = 100;
-  }
-  else {
-    search_site_input.style.zIndex = 1;
+    search_site_input.style.zIndex = 100
+  } else {
+    search_site_input.style.zIndex = 1
   }
 }
 
-function deletePostit(postitNum) {
+function deletePostit(event) {
   console.log("deletePositit")
-  const POSTIT_ID = `postit${postitNum}`
-  const POSTIT_DIV = document.getElementById(POSTIT_ID)
-  while (POSTIT_DIV.firstChild) {
-    POSTIT_DIV.removeChild(POSTIT_DIV.firstChild)
-  }
-  const postitTitle = document.createElement("h4")
-  postitTitle.setAttribute("class", "card-title")
-  postitTitle.innerText = "Postit"
-  POSTIT_DIV.appendChild(postitTitle)
-  const cleanPostits = postits.filter(function(postit) {
-    return postit.id !== POSTIT_ID
-  })
+  const div = event.target.parentNode
+  div.parentNode.removeChild(div)
+  const cleanPostits = postits.filter((_, index) => index !== 0)
   console.log(cleanPostits)
   postits = cleanPostits
   savePostits()
@@ -134,44 +120,89 @@ function savePostits() {
   localStorage.setItem(POSTIT_LS, JSON.stringify(postits))
 }
 
-function paintPostit(postitNum, contents) {
-  deletePostit(postitNum)
-  const POSTIT_ID = `postit${postitNum}`
-  const POSTIT_DIV = document.getElementById(POSTIT_ID)
-  const postitTitle = document.createElement("h4")
-  const delBtn = document.createElement("button")
-  const span = document.createElement("span")
-  postitTitle.setAttribute("class", "card-title")
-  postitTitle.innerText = "Postit"
-  delBtn.innerText = "❌"
-  delBtn.addEventListener("click", function() {
-    deletePostit(postitNum)
-  })
-  delBtn.setAttribute("class", "btn-sm btn-dark pull-right col-md-6 col-md-offset-3 centered")
-  span.innerText = contents
-  //   POSTIT_DIV.appendChild(postitTitle)
-  POSTIT_DIV.appendChild(span)
-  POSTIT_DIV.appendChild(delBtn)
-  const postitObj = {
-    text: contents,
-    id: POSTIT_ID
-  }
-  postits.unshift(postitObj)
-  console.log(postits)
-  savePostits()
-  console.log("paintPostit")
-}
+// function paintPostit(postitNum, contents) {
+//   const POSTIT_ID = `postit${postitNum}`
+//   const POSTIT_DIV = document.getElementById(POSTIT_ID)
+//   const postitTitle = document.createElement("h4")
+//   const delBtn = document.createElement("button")
+//   const span = document.createElement("span")
+//   postitTitle.setAttribute("class", "card-title")
+//   postitTitle.innerText = "Postit"
+//   delBtn.innerText = "❌"
+//   delBtn.addEventListener("click", deletePostit)
+//   delBtn.setAttribute("class", "btn-sm btn-dark pull-right col-md-6 col-md-offset-3 centered")
+//   span.innerText = contents
+//   //   POSTIT_DIV.appendChild(postitTitle)
+//   POSTIT_DIV.appendChild(span)
+//   POSTIT_DIV.appendChild(delBtn)
+//   const postitObj = {
+//     text: contents,
+//     id: POSTIT_ID
+//   }
+//   postits.push(postitObj)
+//   console.log(postits)
+//   savePostits()
+//   console.log("paintPostit")
+// }
 
 function loadPostit() {
   const loadedPostits = localStorage.getItem(POSTIT_LS)
   if (loadedPostits !== null) {
+    //   string을 배열로
     const parsedPostits = JSON.parse(loadedPostits)
-    var postitIndex = 1
-    parsedPostits.forEach(function(postit) {
-      paintPostit(postitIndex, postit.text)
-      postitIndex += 1
-    })
+    console.log({ parsedPostits })
+    renderPostitList(parsedPostits)
     console.log("loadPostit")
+  }
+}
+
+function addPostit(postit) {
+  const addedPostits = localStorage.getItem(POSTIT_LS)
+  const parsedAddedPostits = JSON.parse(addedPostits)
+  console.log({ addedPostits })
+  console.log({ parsedAddedPostits })
+  postits = parsedAddedPostits.push(postit)
+  savePostits()
+}
+
+// 멘토링
+function renderPostit(postit) {
+  const POSTIT_DIV = document.createElement("div")
+  const postitTitle = document.createElement("h4")
+  const delBtn = document.createElement("button")
+  const span = document.createElement("span")
+  const newID = postits.length + 1
+  postitTitle.setAttribute("class", "card-title")
+  postitTitle.innerText = "Postit"
+  delBtn.innerText = "❌"
+  // TODO: 삭제버튼 이벤트 만들어야함
+  delBtn.addEventListener("click", deletePostit)
+  delBtn.setAttribute("class", "btn-sm btn-dark pull-right col-md-6 col-md-offset-3 centered")
+  span.innerText = postit.text
+  POSTIT_DIV.setAttribute("id", newID)
+  POSTIT_DIV.setAttribute("class", "card py-4 h-100")
+  POSTIT_DIV.appendChild(postitTitle)
+  POSTIT_DIV.appendChild(span)
+  POSTIT_DIV.appendChild(delBtn)
+  return POSTIT_DIV
+}
+
+function renderPostitList(postits) {
+  const reversedPostitElements = postits.reverse()
+  //   local 내용을 div묶음으로 전부 만들어 배열로 리턴
+  const postitElements = reversedPostitElements.map(renderPostit)
+  console.log({ reversedPostitElements, postitElements })
+  const postitListElement = document.getElementById("postit-list")
+  //   removePostitListAll(postitListElement)
+  postitElements.forEach(element => {
+    // TODO: appendChild가 아니라 postitListElement의 child자체를 바꿔서 쌓이지 않도록 해야함.
+    postitListElement.appendChild(element)
+  })
+}
+
+function removePostitListAll(postitListElement) {
+  while (postitListElement.hasChildNodes()) {
+    postitListElement.removeChild()
   }
 }
 
@@ -180,13 +211,11 @@ function searchFunction() {}
 function handleSubmit() {
   event.preventDefault()
   const currentValue = searchInput.value
-  var currentPostitNum = postits.length
+  const currentPostit = { text: currentValue, id: 0 }
   console.log(currentValue)
   if (currentValue !== "") {
-    console.log("currentPostitNum = ", currentPostitNum)
-    currentPostitNum = (currentPostitNum % POSTIT_COUNT) + 1
-    paintPostit(currentPostitNum, currentValue)
-    // 검색 시작
+    loadPostit()
+    addPostit(currentPostit)
     // saveURL()
   }
   searchInput.value = ""
