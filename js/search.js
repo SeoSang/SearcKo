a2174a3cfc14aa938c06482918652e8d134a2229
2yunsoo <starlee9711@gmail.com>
2020-01-04 13:38:52 +0900

    윤수짱

--- js/search.js
+++ js/search.js
@@ -3,9 +3,35 @@ const search = document.getElementById("search_input"),
   searchForm = document.getElementById("searchForm"),
   postitList = document.querySelector(".js-postitList")
 
-function saveURL() {}
+function saveURL() {
+  search_content = "https://www.google.com/search?q=" + search.value;
+  if(search_contain.value) {
+    search_content = search_content + ' "' + search_contain.value + '"'
+    search_input.value = search_input.value + " " + search_contain.value;
+  }
+  if(search_except.value)
+    search_content = search_content + ' -' + search_except.value
+  if(search_synonym.value)
+    search_content = search_content + ' ~' + search_synonym.value
+  if(search_site.value)
+    search_content = search_content + ' site:' + search_site.value
+  if(search_filetype.value)
+    search_content = search_content + ' filetype:' + search_filetype.value
 
-function saveKeyWord() {}
+  location.href = search_content;
+}
+
+document.getElementById("search_input").onblur = function() {
+  searchBasic = search_input.value;
+}
+
+document.getElementById("search_contain").onblur = function() {
+  search_input.value = searchBasic + " " + search_contain.value;
+}
+
+function saveKeyWord() {
+
+}
 
 function paintPostit() {
   const span = document.createElement("span")
@@ -19,6 +45,7 @@ function paintPostit() {
 
 function searchFunction() {}
 function init() {
+  searchForm.addEventListener("submit", saveURL)
   searchForm.addEventListener("submit", paintPostit)
 }
 init()
