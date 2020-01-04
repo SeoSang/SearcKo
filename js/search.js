4347161e6f9e58fb587686c3f9b1e153091de0ef
SeoSang <ddrrpg@naver.com>
2020-01-04 12:49:21 +0900

    폼 넘기는 방식

--- js/search.js
+++ js/search.js
@@ -1,6 +1,24 @@
-document.getElementById("searchForm").onsubmit = function() {
-  return searchFunction()
+const search = document.getElementById("search_input"),
+  searchContents = search.value,
+  searchForm = document.getElementById("searchForm"),
+  postitList = document.querySelector(".js-postitList")
+
+function saveURL() {}
+
+function saveKeyWord() {}
+
+function paintPostit() {
+  const span = document.createElement("span")
+  const postitContent = searchContents
+  console.log(postitContent)
+  console.log(search)
+  span.innerText = postitContent
+  postitList.appendChild(span)
+  console.log("paintPostit")
 }
+
 function searchFunction() {}
-function init() {}
+function init() {
+  searchForm.addEventListener("submit", paintPostit)
+}
 init()
